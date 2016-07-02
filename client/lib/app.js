ce0e8317f38d63bf80d348be3f68c2add93c0839
Hadar Gorodesky <hadarg@aperio-manage.com>
2016-07-02 13:44:34 +0300

    Initial build of motogat Add index and app files

---
+++ client/lib/app.js
@@ -0,0 +1,54 @@
+//import angular from 'angular';
+//import angularMeteor from 'angular-meteor';
+//import uiRouter from 'angular-ui-router';
+//import 'bootstrap/dist/css/bootstrap.css';
+
+//import { Meteor } from 'meteor/meteor';
+
+//angular.module('brgo', ['angularMeteor','ui.router','ui.bootstrap']);
+//angular.module('brgo', ['angularMeteor','uiRouter','ui.bootstrap']);
+
+//function onReady() {
+//  angular.bootstrap(document, ['brgo'], {
+//    strictDi: true
+//  });
+
+//}
+
+//if (Meteor.isCordova)
+//  angular.element(document).on("deviceready", onReady);
+//else
+//  angular.element(document).ready(onReady);
+
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+//import angularBootstrap from 'angular-ui-bootstrap';
+//import angularUiRouter from 'angular-ui-router'
+//import angularPromiser from 'angular-meteor-promiser';
+//import 'angular-filter';
+//import uiRouter from 'angular-ui-router';
+//import 'bootstrap/dist/css/bootstrap.css';
+
+//import { Meteor } from 'meteor/meteor';
+
+//angular.module('brgo', ['angularMeteor','ui.router','ui.bootstrap']);
+//angular.module('brgo', [angularMeteor, 'ui.router','accounts.ui','ui.bootstrap']);
+//angular.module('motogat', [angularMeteor,angularUiRouter,'accounts.ui','angular.filter',angularBootstrap,angularPromiser]);
+angular.module('motogat', [angularMeteor]);
+
+
+function onReady() {
+  angular.bootstrap(document, ['motogat'], {
+    strictDi: true
+  });
+
+}
+
+//Accounts.ui.config({
+//  passwordSignupFields: "USERNAME_AND_EMAIL"
+//});
+
+if (Meteor.isCordova)
+  angular.element(document).on("deviceready", onReady);
+else
+  angular.element(document).ready(onReady);
